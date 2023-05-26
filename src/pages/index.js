c2eedeafda8cc39b7a8a7eaa41b5de9b7e7651db
Abdulmelik Kalkan <49363216+AbdulmelikKalkan@users.noreply.github.com>
2023-05-26 22:18:45 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,7 +40,7 @@ export default function Home() {
         </div>
 
         <div className={styles.center}>
-          HOLA
+          GitOPS
           <Image
             className={styles.logo}
             src="/next.svg"
